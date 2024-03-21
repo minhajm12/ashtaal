5925ebf03c4eb1f69d902a78a8a9981951fbfafb
minhajm12 <minhajatholi222@gmail.com>
2024-03-22 01:16:40 +0530

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,56 @@ document.documentElement.style.setProperty('--header-height', `${headerHeight}px
 document.documentElement.style.setProperty('--main-height', `${mainHeight}px`);
 document.documentElement.style.setProperty('--banner-height', `${bannerHeight}px`);
 // console.log(bannerHeight);
+
+
+
+
+
+
+
+
+
+// const text = document.querySelector('.text p');
+// text.innerHTML = text.innerText.split('').map(
+// (char, i) =>
+// `<span style="transform:rotate(${i * 8.3}deg)">${char}</span>`
+// ).join('');
+
+
+$(document).ready(function(){
+    $('.main-slider-colums').owlCarousel({
+        margin:20,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        // infinite:true,
+        loop:true,
+        nav: true,
+        touchDrag: false,
+        mouseDrag: false,
+        // dots:true,
+        // dotsEach: true,
+        // autoplay:true,
+        slideToScroll:1,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1300:{
+                items:1
+            },
+            1500:{
+                items:1
+            }
+        }
+      })
+});
+
+
+
+
+
+
+
