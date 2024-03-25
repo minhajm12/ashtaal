6ae8a109555bf401b4b1f744c7e52dea3b7d90e8
minhajm12 <minhajatholi222@gmail.com>
2024-03-25 13:14:47 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,33 @@
 const headerHeight = document.getElementById('header').offsetHeight;
 const mainHeight = document.getElementById('main-banner').offsetHeight;
 const bannerHeight = document.getElementById('banner-section').offsetHeight;
+const projectHeight1 = document.getElementById('project-items1').offsetHeight;
+const projectHeight2 = document.getElementById('project-items2').offsetHeight;
+const projectHeight3 = document.getElementById('project-items3').offsetHeight;
+const projectHeight4 = document.getElementById('project-items4').offsetHeight;
+const projectHeight5 = document.getElementById('project-items5').offsetHeight;
+const projectHeight6 = document.getElementById('project-items6').offsetHeight;
+
+const total_height = projectHeight1 + projectHeight2 + projectHeight3 + projectHeight4 + projectHeight5 + projectHeight6;
 
 
 document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
 document.documentElement.style.setProperty('--main-height', `${mainHeight}px`);
 document.documentElement.style.setProperty('--banner-height', `${bannerHeight}px`);
-// console.log(bannerHeight);
+document.documentElement.style.setProperty('--project-height', `${total_height}px`);
+// document.documentElement.style.setProperty('--project-height2', `${projectHeight2}px`);
+// document.documentElement.style.setProperty('--project-height3', `${projectHeight3}px`);
+// document.documentElement.style.setProperty('--project-height4', `${projectHeight4}px`);
+// document.documentElement.style.setProperty('--project-height5', `${projectHeight5}px`);
+// document.documentElement.style.setProperty('--project-height6', `${projectHeight6}px`);
+console.log(total_height);
+// console.log(projectHeight2);
+// console.log(projectHeight3);
+// console.log(projectHeight4);
+// console.log(projectHeight5);
+// console.log(projectHeight6);
+
+
 
 
 
